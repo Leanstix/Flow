3070b2b9ae184ec0cec03ef5afcc3e92c8efec28
Leanstix <leanstixx@gmail.com>
2024-12-19 10:03:23 +0100

    fix

--- flow/marketplace/urls.py
+++ flow/marketplace/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from .views import SendMessageView,CustomerMessagesView, AdvertisementListView, AdvertisementCreateView, AdvertisementDetailView, SellerMessagesView, ReplyToMessageView
 
 urlpatterns = [
-    path("messages/<int:pk>/send/", SendMessageView.as_view(), name="send_message"),
+    path("messages/send/", SendMessageView.as_view(), name="send_message"),
     path("", AdvertisementListView.as_view(), name="advertisement_list"),
     path("create/", AdvertisementCreateView.as_view(), name="advertisement_create"),
     path("<int:pk>/", AdvertisementDetailView.as_view(), name="advertisement_detail"),
