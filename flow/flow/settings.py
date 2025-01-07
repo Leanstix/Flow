67acb833eb83c298563c61321b6410fde0c1abd8
Leanstix <leanstixx@gmail.com>
2025-01-07 13:58:34 +0100

    setting up emailing system"

--- flow/flow/settings.py
+++ flow/flow/settings.py
@@ -146,11 +146,16 @@ WSGI_APPLICATION = 'flow.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
