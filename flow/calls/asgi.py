9e49473c32f45ee635d6a733f9fa265bac6a467e
Leanstix <leanstixx@gmail.com>
2025-01-14 13:49:15 +0100

    still working n calls

--- flow/calls/asgi.py
+++ flow/calls/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import flow.routing
+from flow.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow.settings')
 
@@ -10,7 +10,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            flow.routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
