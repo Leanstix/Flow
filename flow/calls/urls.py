6d2676305c120fa52b8988799e16ea4f8269faa4
Leanstix <leanstixx@gmail.com>
2025-01-15 03:56:45 +0100

    setting up calls urls

--- flow/calls/urls.py
+++ flow/calls/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import create_room, join_room
 
 urlpatterns = [
-    path('create-room/', create_room, name='create_room'),
-    path('join-room/<str:room_name>/', join_room, name='join_room'),
+    path('create-room/', create_room.as_view(), name='create_room'),
+    path('join-room/<str:room_name>/', join_room.as_view(), name='join_room'),
 ]
