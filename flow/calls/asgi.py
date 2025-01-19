13092305c513aa67d63c91ebd348bc1ec2142d22
Leanstix <leanstixx@gmail.com>
2025-01-19 04:15:36 +0100

    added authentication to the websocket

--- flow/calls/asgi.py
+++ flow/calls/asgi.py
@@ -3,14 +3,19 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from flow.routing import websocket_urlpatterns
+from channels.security.websocket import AllowedHostsOriginValidator
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
+    "websocket":  AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    )
+    
 })
