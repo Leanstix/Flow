d07520a4f791d89ebfe6051391c9e52be9fc6172
Leanstix <leanstixx@gmail.com>
2025-01-29 13:48:09 +0100

    removed saving to local storage first and just up-load to drive directly

--- flow/userauth/views.py
+++ flow/userauth/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer, UserActivationSerializer, UserProfileUpdateSerializer, ChangePasswordSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import UpdateAPIView
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 from rest_framework.permissions import AllowAny
 from .models import User
 from .drive_utils import upload_file_to_drive
@@ -31,7 +31,7 @@ class UserActivationView(APIView):
             return Response({"message": "Account activated successfully!"}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-User = get_user_model()
+#User = get_user_model()
 
 class UserProfileUpdateView(UpdateAPIView):
     serializer_class = UserProfileUpdateSerializer
