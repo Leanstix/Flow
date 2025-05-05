2f0ba19ea867085356c6b569ccbdc05dfa39097b
Leanstix <leanstixx@gmail.com>
2025-05-05 13:33:51 +0100

    connecting primarily to online redis server

--- flow/flow/settings.py
+++ flow/flow/settings.py
@@ -168,7 +168,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.getenv("REDIS_URL", "redis://localhost:6379")],
+            "hosts": [os.getenv(REDIS)],
         },
     },
 }
