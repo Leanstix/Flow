cbd79365b412789a028dc00ba7cdc14ca2a170f6
Aleshinloye Olamilekan <leanstixx@gmail.com>
2026-07-10 18:37:28 +0100

    Add message replies edits deletion and receipt endpoints

--- flow/messaging/views.py
+++ flow/messaging/views.py
@@ -1,14 +1,25 @@
 from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.db.models import Count
+from django.utils import timezone
 from rest_framework import serializers, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Conversation, Message
-from .serializers import ConversationSerializer, MessageSerializer
-from .services import broadcast_message, create_message_notifications
+from .serializers import ConversationSerializer, MessageEditSerializer, MessageSerializer
+from .services import (
+    broadcast_message,
+    broadcast_message_deleted,
+    broadcast_message_updated,
+    create_message_notifications,
+    ensure_message_receipts,
+    hydrated_message,
+    mark_conversation_delivered,
+    mark_conversation_read,
+)
 
 User = get_user_model()
 
@@ -77,9 +88,11 @@ class ConversationViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['get'])
     def messages(self, request, pk=None):
         conversation = self.get_object()
+        mark_conversation_delivered(conversation, request.user)
         recent_messages = list(
             Message.objects.filter(conversation=conversation)
-            .select_related('sender')
+            .select_related('sender', 'reply_to', 'reply_to__sender')
+            .prefetch_related('receipts')
             .order_by('-timestamp')[:50]
         )
         serializer = MessageSerializer(reversed(recent_messages), many=True)
@@ -90,26 +103,39 @@ class ConversationViewSet(viewsets.ModelViewSet):
         conversation = self.get_object()
         serializer = MessageSerializer(data={**request.data, 'conversation': conversation.id})
         serializer.is_valid(raise_exception=True)
-        message = serializer.save(conversation=conversation, sender=request.user)
-        broadcast_message(message)
-        create_message_notifications(message)
-        return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
+
+        with transaction.atomic():
+            message = serializer.save(conversation=conversation, sender=request.user)
+            ensure_message_receipts(message)
+            create_message_notifications(message)
+            transaction.on_commit(lambda: broadcast_message(message))
+
+        return Response(MessageSerializer(hydrated_message(message)).data, status=status.HTTP_201_CREATED)
 
     @action(detail=True, methods=['post'])
     def mark_read(self, request, pk=None):
         conversation = self.get_object()
-        updated_count = conversation.messages.filter(is_read=False).exclude(sender=request.user).update(is_read=True)
+        updated_count = mark_conversation_read(conversation, request.user)
         return Response({'updated_count': updated_count}, status=status.HTTP_200_OK)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'patch', 'put', 'delete', 'head', 'options']
+
+    def get_serializer_class(self):
+        if self.action in {'update', 'partial_update'}:
+            return MessageEditSerializer
+        return MessageSerializer
 
     def get_queryset(self):
         if getattr(self, 'swagger_fake_view', False):
             return Message.objects.none()
-        return Message.objects.filter(conversation__participants=self.request.user).select_related('sender', 'conversation')
+        return Message.objects.filter(
+            conversation__participants=self.request.user
+        ).select_related(
+            'sender', 'conversation', 'reply_to', 'reply_to__sender'
+        ).prefetch_related('receipts').distinct()
 
     def perform_create(self, serializer):
         conversation_id = self.request.data.get('conversation')
@@ -121,6 +147,32 @@ class MessageViewSet(viewsets.ModelViewSet):
         except Conversation.DoesNotExist:
             raise ValidationError({'conversation': 'Invalid conversation ID or you are not a participant.'})
 
-        message = serializer.save(sender=self.request.user, conversation=conversation)
-        broadcast_message(message)
-        create_message_notifications(message)
+        with transaction.atomic():
+            message = serializer.save(sender=self.request.user, conversation=conversation)
+            ensure_message_receipts(message)
+            create_message_notifications(message)
+            transaction.on_commit(lambda: broadcast_message(message))
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        message = self.get_object()
+        if message.sender_id != request.user.id:
+            raise PermissionDenied('Only the sender can edit this message.')
+        if message.deleted_at:
+            raise ValidationError({'detail': 'Deleted messages cannot be edited.'})
+
+        serializer = MessageEditSerializer(message, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save(edited_at=timezone.now())
+        transaction.on_commit(lambda: broadcast_message_updated(message))
+        return Response(MessageSerializer(hydrated_message(message)).data)
+
+    def destroy(self, request, *args, **kwargs):
+        message = self.get_object()
+        if message.sender_id != request.user.id:
+            raise PermissionDenied('Only the sender can delete this message.')
+        if not message.deleted_at:
+            message.deleted_at = timezone.now()
+            message.save(update_fields=['deleted_at'])
+            transaction.on_commit(lambda: broadcast_message_deleted(message))
+        return Response(status=status.HTTP_204_NO_CONTENT)
