45cb8545ecb820924fbffb3caa6983097045a157
Aleshinloye Olamilekan <leanstixx@gmail.com>
2026-07-10 15:48:19 +0100

    Expose clean group API paths

--- flow/communities/urls.py
+++ flow/communities/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CommunityPostViewSet, CommunityResourceViewSet, CommunityViewSet
 
 router = DefaultRouter()
-router.register(r'groups', CommunityViewSet, basename='community')
 router.register(r'posts', CommunityPostViewSet, basename='community-post')
 router.register(r'resources', CommunityResourceViewSet, basename='community-resource')
+router.register(r'', CommunityViewSet, basename='community')
 
 urlpatterns = [path('', include(router.urls))]
