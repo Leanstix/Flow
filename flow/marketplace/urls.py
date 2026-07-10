2c569600d41803aee95b32450e9a0c61a731a4cb
Aleshinloye Olamilekan <leanstixx@gmail.com>
2026-07-10 15:42:37 +0100

    Expose marketplace listing routes

--- flow/marketplace/urls.py
+++ flow/marketplace/urls.py
@@ -1,18 +1,9 @@
-from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-from .views import SendMessageView, ReportAdvertisementView, ConversationMessagesView, CustomerMessagesView, AdvertisementListView, AdvertisementCreateView, AdvertisementDetailView, SellerMessagesView, ReplyToMessageView
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path("messages/send/", SendMessageView.as_view(), name="send_message"),
-    path("", AdvertisementListView.as_view(), name="advertisement_list"),
-    path("create/", AdvertisementCreateView.as_view(), name="advertisement_create"),
-    path("<int:pk>/", AdvertisementDetailView.as_view(), name="advertisement_detail"),
-    path("messages/seller/", SellerMessagesView.as_view(), name="seller_messages"),
-    path("messages/reply/", ReplyToMessageView.as_view(), name="reply_to_message"),
-    path("messages/customer/", CustomerMessagesView.as_view(), name="customer_messages"),
-    path("advertisements/report/", ReportAdvertisementView.as_view(), name="report_advertisement"),
-    path("messages/conversations/<uuid:conversation_id>/", ConversationMessagesView.as_view(), name="conversation_messages"),
-]
+from .views import AdvertisementViewSet
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+router = DefaultRouter()
+router.register(r'listings', AdvertisementViewSet, basename='marketplace-listing')
+
+urlpatterns = [path('', include(router.urls))]
