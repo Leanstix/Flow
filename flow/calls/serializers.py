26b684c66ea80e8ad83204919a7ec6d8d5d1d756
Aleshinloye Olamilekan <leanstixx@gmail.com>
2026-07-11 04:59:09 +0100

    Serialize call sessions and invitations

--- flow/calls/serializers.py
+++ flow/calls/serializers.py
@@ -1,17 +1,44 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Room
+from rest_framework import serializers
+
+from .models import CallInvitation, Room
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
+
+class CallUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'email', 'user_name', 'profile_picture']
+
+
+class CallInvitationSerializer(serializers.ModelSerializer):
+    user = CallUserSerializer(read_only=True)
+    invited_by = CallUserSerializer(read_only=True)
+
+    class Meta:
+        model = CallInvitation
+        fields = ['id', 'user', 'invited_by', 'status', 'created_at', 'responded_at']
+
 
 class RoomSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(many=True, read_only=False)  # Nested serialization
+    created_by = CallUserSerializer(read_only=True)
+    participants = CallUserSerializer(many=True, read_only=True)
+    invitations = CallInvitationSerializer(many=True, read_only=True)
+    conversation_id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Room
-        fields = ['id', 'room_name', 'created_at', 'participants']
+        fields = [
+            'id',
+            'room_name',
+            'created_by',
+            'conversation_id',
+            'call_type',
+            'status',
+            'participants',
+            'invitations',
+            'created_at',
+            'started_at',
+            'ended_at',
+        ]
