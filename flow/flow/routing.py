83c638875a20fdeb61c306748ddcf6ca976d3ff2
Aleshinloye Olamilekan <leanstixx@gmail.com>
2026-07-11 05:00:50 +0100

    Route private incoming call websocket

--- flow/flow/routing.py
+++ flow/flow/routing.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path
 
-from calls.consumers import CallConsumer
+from calls.consumers import CallConsumer, UserCallConsumer
 from messaging.consumers import ConversationConsumer
 from notifications.consumers import NotificationConsumer
 
 
 websocket_urlpatterns = [
     re_path(r'ws/call/(?P<room_name>[-\w]+)/$', CallConsumer.as_asgi()),
+    path('ws/calls/', UserCallConsumer.as_asgi()),
     path('ws/conversations/<int:conversation_id>/', ConversationConsumer.as_asgi()),
     path('ws/notifications/', NotificationConsumer.as_asgi()),
 ]
